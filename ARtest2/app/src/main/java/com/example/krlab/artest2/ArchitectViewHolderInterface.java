175772becb773d23e58db6ad1c555c810c5570cd
huncro <crow417.shiina@gmail.com>
2017-11-17 01:46:03 +0900

    'ARが表示できない！'

---
+++ ARtest2/app/src/main/java/com/example/krlab/artest2/ArchitectViewHolderInterface.java
@@ -0,0 +1,12 @@
+package com.example.krlab.artest2;
+
+/**
+ * Created by krlab on 2017/11/16.
+ */
+
+public interface ArchitectViewHolderInterface {
+    public static interface ILocationProvider {
+        public void onResume();
+        public void onPause();
+    }
+}
--- ARtest2/app/src/main/java/com/example/krlab/artest2/MainActivity.java
+++ ARtest2/app/src/main/java/com/example/krlab/artest2/MainActivity.java
@@ -1,43 +1,88 @@
 package com.example.krlab.artest2;
 
 //11/12
+
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
 import com.wikitude.architect.ArchitectStartupConfiguration;
 import com.wikitude.architect.ArchitectView;
+import com.wikitude.architect.ArchitectView.SensorAccuracyChangeListener;
+import com.wikitude.common.camera.CameraSettings;
+
 import java.io.IOException;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements ArchitectViewHolderInterface {
+//public class MainActivity extends AppCompatActivity {
     protected ArchitectView     architectView;  //11/12
+    protected Location          lastKnownLocation;  //11/16
+    protected SensorAccuracyChangeListener  sensorAccuracyListener;
+    protected LocationListener  locationListener;
+    protected ArchitectViewHolderInterface.ILocationProvider    locationProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //11/12(ねっくだったとこ)
-        this.architectView = (ArchitectView)this.findViewById( R.id.architectView );
+        this.architectView = (ArchitectView) this.findViewById(R.id.architectView);
         final ArchitectStartupConfiguration config = new ArchitectStartupConfiguration();
         config.setLicenseKey(this.getWikitudeSDKLicenseKey());
-        this.architectView.onCreate( config );
-        //final StartupConfiguration config = new StartupConfiguration(this.getWikitudeSDKLicenseKey());
-                //this.getWikitudeSDKLicenseKey(),
-                //Features.Geo,
-                //config.getCameraPosition()
-        //);  //下2行アレンジ
 
-        //11/12下全部
+        //config.setFeatures(ArchitectStartupConfiguration.Features.Geo);
+        //config.getCameraPosition();
+
+
+        //final ArchitectStartupConfiguration config = new ArchitectStartupConfiguration(this.getWikitudeSDKLicenseKey(), ArchitectStartupConfiguration.Features.Geo, config.getCameraPosition());
+
+
         try {
-            this.architectView.onCreate((ArchitectStartupConfiguration) config);
+            this.architectView.onCreate(config);
         } catch (RuntimeException ex) {
             this.architectView = null;
             Toast.makeText(getApplicationContext(), "can't create Architect View", Toast.LENGTH_SHORT).show();
         }
+
+/*
+        //11/16
+        this.sensorAccuracyListener = this.getSensorAccuracyListener();
+        this.locationListener = new LocationListener() {
+            @Override
+            public void onStatusChanged( String provider, int status, Bundle extras ) {
+            }
+
+            @Override
+            public void onProviderEnabled( String provider ) {
+            }
+
+            @Override
+            public void onProviderDisabled( String provider ) {
+            }
+
+            @Override
+            public void onLocationChanged(final Location location) {
+                if (location != null) {
+                    MainActivity.this.lastKnownLocation = location;
+                    if (MainActivity.this.architectView != null) {
+                        if (location.hasAltitude() && location.hasAccuracy() &&location.getAccuracy() < 7) {
+                            MainActivity.this.architectView.setLocation(location.getLatitude(), location.getLongitude(), location.getAltitude(), location.getAccuracy());
+                        } else {
+                            MainActivity.this.architectView.setLocation(location.getLatitude(), location.getLongitude(), location.hasAccuracy() ? location.getAccuracy() : 1000);
+                        }
+                    }
+                }
+            }
+        };
+        this.locationProvider = getLocationProvider(this.locationListener);
+        */
     }
 
+    //11/12下全部
     @Override
     protected void onPostCreate( final Bundle savedInstanceState ) {
         super.onPostCreate(savedInstanceState);
@@ -47,7 +92,6 @@ public class MainActivity extends AppCompatActivity {
             this.architectView.onPostCreate();
             try {
                 this.architectView.load( this.getARchitectWorldPath());
-                this.architectView.setCullingDistance(50 * 1000);   //50k
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
@@ -57,6 +101,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        //11/16
+        if ( this.architectView != null ) {
+            this.architectView.onResume();
+            if ( this.sensorAccuracyListener != null ) {
+                this.architectView.registerSensorAccuracyChangeListener( this.sensorAccuracyListener );
+            }
+        }
         if (this.architectView != null) {
             this.architectView.onResume();
         }
@@ -65,6 +116,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        //11/16
+        if ( this.architectView != null ) {
+            this.architectView.onPause();
+            if (this.sensorAccuracyListener!=null) {
+
+                this.architectView.unregisterSensorAccuracyChangeListener(this.sensorAccuracyListener);
+            }
+        }
         if (this.architectView != null) {
             this.architectView.onPause();
         }
@@ -73,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
         if (this.architectView != null) {
             this.architectView.onDestroy();
         }
@@ -85,4 +145,30 @@ public class MainActivity extends AppCompatActivity {
     protected String getARchitectWorldPath() {
         return "wikitude/index.html";
     }
+
+    protected CameraSettings.CameraPosition getCameraPosition() {
+        return CameraSettings.CameraPosition.BACK;
+    }
+
+
+    //11/16
+    public ArchitectViewHolderInterface.ILocationProvider getLocationProvider(final LocationListener locationListener) {
+        return new LocationProvider(this, locationListener);
+    }
+/*
+    private long lastCalibrationToastShownTimeMillis = System.currentTimeMillis();
+
+    public ArchitectView.SensorAccuracyChangeListener getSensorAccuracyListener() {
+        return new ArchitectView.SensorAccuracyChangeListener() {
+            @Override
+            public void onCompassAccuracyChanged( int accuracy ) {
+				// UNRELIABLE = 0, LOW = 1, MEDIUM = 2, HIGH = 3
+                if ( accuracy < SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM && MainActivity.this != null && !MainActivity.this.isFinishing() && System.currentTimeMillis() - MainActivity.this.lastCalibrationToastShownTimeMillis > 5 * 1000) {
+                    //Toast.makeText( MainActivity.this, R.string.compass_accuracy_low, Toast.LENGTH_LONG ).show();
+                    MainActivity.this.lastCalibrationToastShownTimeMillis = System.currentTimeMillis();
+                }
+            }
+        };
+    }
+*/
 }
